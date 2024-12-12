4e43570f0d23d52d47def1afc2884bb804d49039
Hasansarwer <hasanmathju@gmail.com>
2024-12-13 02:31:59 +0600

    complete

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import './EventPlanner.css'
+
+const Footer = () => {
+    return(
+        <>
+        <footer>
+            <p>&copy; Event Planeer Organization. All rights rederved.</p>
+        </footer>
+        </>
+    )
+
+}
+
+export default Footer;
